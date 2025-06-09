5f7ac0197769945ce1c24b163cc07b43d096952b
Jared Norman <jared@super.gd>
2025-06-09 14:00:30 -0700

    Move decorators out of autoloaded location

--- lib/solidus_active_shipping/engine.rb
+++ lib/solidus_active_shipping/engine.rb
@@ -22,7 +22,7 @@ module SolidusActiveShipping
         Rails.env.production? ? require(c) : load(c)
       end
 
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/decorators/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
